335fcdf3d70ec4c5da991f880b31821c1245c1c7
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-09-19 17:35:36 -0500

    empezando con el uso de archivos estaticos

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
+const path = require('path')
 
 const PORT = 4000
+
+app.use( "/",express.static(path.join( __dirname,'/public')))
+
+
+// app.get("/", (req, res)=>{
+//   res.send("hello world")
+// })
 
 app.listen(PORT, ()=>{
   console.log(`Server running on port ${PORT}`);
