e621961cfec4ac4529970c29b985ec8e9c6b0263
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-09-22 14:59:15 -0500

    creacion de tareas

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const path = require('path')
 
 const mongoose = require('./db/database')
 
-const PORT = 4000
+app.use(cors());
+app.use(express.json());
 
-app.use( "/",express.static(path.join( __dirname,'/client')))
+const PORT = 4000
+ 
+app.use("/", require('./routes/router.route'))
 
 app.listen(PORT, ()=>{
   console.log(`Server running on port ${PORT}`);
---
+++ routes/router.route.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+const Task = require("../db/schemas");
+
+router.post("/", (req, res) => {
+  const { title, description, done } = req.body;
+  const TASK = new Task({ title, description, done });
+  try {
+    TASK.save();
+    res.json({ status: "ok" });
+    console.log("task saved");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+module.exports = router;
