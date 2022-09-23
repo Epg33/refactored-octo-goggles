2f75386165be4a30462731fb836b988a968e5371
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-09-23 15:37:48 -0500

    validacion de la existencia de un usuario y listado de usuarios

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,13 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const {user} = require('../db/schema')
+const { user } = require("../db/schema");
 
-router.post('/', async (req, res)=>{
-  const {name, email, password} = req.body;
-  const USER = new user({name, email, password});
-  await USER.save();
-  res.send({message: 'usuario registrado'})
+router.get('/', async (req, res)=>{
+  const users = await user.find();
+  res.send(users)
 })
 
-module.exports=router;
+router.post("/", async (req, res) => {
+  const { name, email, password } = req.body;
+  const userDone = await user.findOne({ email: email });
+  const USER = new user({ name, email, password });
+  if (userDone) {
+    res.send({ message: "alredy existing user" });
+  } else {
+    await USER.save();
+    res.send({ message: "usuario registrado" });
+  }
+});
+
+module.exports = router;
