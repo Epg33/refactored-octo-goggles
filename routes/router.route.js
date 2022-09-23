a3af17bd76bcdbdb44b9a7138ace742eb6f1ea64
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-09-22 19:10:39 -0500

    actualizar tareaS

--- routes/router.route.js
+++ routes/router.route.js
@@ -3,18 +3,18 @@ const router = express.Router();
 
 const Task = require("../db/schemas");
 
-router.get('/', async (req, res)=>{
+router.get("/", async (req, res) => {
   const homeWork = await Task.find();
   res.send(homeWork);
   // console.log("tareas");
-})
+});
 
-router.get('/:id', async (req, res)=>{
-    const id = req.params.id;
-    const homeWork = await Task.findById(id);
-    res.send(homeWork);
-    // console.log("la tarea");
-})
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  const homeWork = await Task.findById(id);
+  res.send(homeWork);
+  // console.log("la tarea");
+});
 
 router.post("/", (req, res) => {
   const { title, description, done } = req.body;
@@ -26,6 +26,18 @@ router.post("/", (req, res) => {
   } catch (err) {
     console.log(err);
   }
+});
+
+router.put("/:id", async (req, res) => {
+  const {title, description, done} = req.body;
+  const TASK = {title, description, done};
+  await Task.findByIdAndUpdate(req.params.id, TASK);
+  console.log('todo bien')
+  res.send({status: 'ok'})
+});
+
+router.delete('/:id', async (req, res)=>{
+
 });
 
 module.exports = router;
