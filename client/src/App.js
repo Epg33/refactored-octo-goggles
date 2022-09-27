0fe9331cf08328981377d782a0e5fd79ebac45a1
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-09-27 12:19:09 -0500

    routing inicial

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './components/Home/index'
+import Register from './components/Register/index'
+import Login from './components/Login/index'
+import ViewTasks from './components/Tasks/ViewTasks'
 import {useState} from 'react'
 
 function App() {
-
-  const [use, setUse] = useState()
   return (
-    <></>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/register' element={<Register />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/account/tasks' element={<ViewTasks />}></Route>
+          <Route ></Route>
+          <Route ></Route>
+          <Route ></Route>
+          <Route ></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
