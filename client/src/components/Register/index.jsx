7447f4d918cd52f408644453f713ae5424bbdc93
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-10-01 18:27:36 -0500

    registro del usuario posible, implementar expresion regular de manera correcta

--- client/src/components/Register/index.jsx
+++ client/src/components/Register/index.jsx
@@ -7,13 +7,28 @@ function Index() {
   const name = useRef();
   const email = useRef();
   const password = useRef();
+  const $ = (e) => {return e.current.value}
 
   const handleSubmit =(e)=>{
-    e.preventDefault()
+    e.preventDefault();
+    if ($(name).length < 3) {
+      alert("Nombre no debe ser menor a 3 caracteres");
+    } 
+     else if ($(password).length < 6) {
+      alert("Password no debe ser menor a 6 caracteres");
+    } else {
+      requestRegisterUser();
+    }
+    
   }
 
   const requestRegisterUser = () =>{
-
+    axios.post('http://localhost:4000/register', {
+      name: name.current.value,
+      email: email.current.value,
+      password: password.current.value
+    })
+    .then(res =>console.log(res))
   }
 
   return (
@@ -22,7 +37,7 @@ function Index() {
       <div>
         <form>
           <h2>Registro</h2>
-          <input type='text' pattern="[a-zA-Z]++" minLength={3} required ref={name}/>
+          <input type='text' minLength={3} required ref={name}/>
           <input type='email' required ref={email}/>
           <input type='password' minLength={6} required ref={password}/>
           <input type='submit' onClick={handleSubmit}/>
