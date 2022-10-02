6efc5e5c6d55148fef4f104ddf6e589b3f828de1
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-10-02 15:00:58 -0500

    validacion de fromato de correo en login

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -1,17 +1,29 @@
-import React from "react";
+import React, {useRef} from "react";
 import Nav from "../Nav";
+import axios from 'axios'
 import "../../styles/login/login.css";
 
 function Index() {
+  const regEx = new RegExp("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$")
+  const email = useRef();
+  const password = useRef();
+
+  const validateLogin = (e) => {
+    e.preventDefault();
+    if(!regEx.test(email.current.value)){
+      alert("ingrese un correo valido")
+    }
+  }
+
   return (
     <>
       <Nav />
       <div>
         <form>
           <h2>Log in</h2>
-          <input type="email" required />
-          <input type="password" required />
-          <input type="submit" />
+          <input type="email" required ref={email} />
+          <input type="password" required ref={password}/>
+          <input type="submit" onClick={validateLogin}/>
         </form>
       </div>
     </>
