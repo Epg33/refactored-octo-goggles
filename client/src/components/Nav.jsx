92304a1642822c421c9c0c74bfd8ad3c6124ca4a
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-10-09 13:58:05 -0500

    aspecto del boton y cambio de clases

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import { useRef } from 'react'
 import {NavLink} from 'react-router-dom'
 import '../styles/nav.css'
 
 function Nav() {
+  const boton = useRef();
+  const theClass = () => {
+    boton.current.classList.toggle('open')
+  } 
   if(window.innerWidth<550){
     return(
       <>
-        <nav>
+        <nav className='nav'>
           <div className='burger-container'>
             <div className='burguer'>
-              <button>
+              <button onClick={theClass} ref={boton}>
                 <span className='top-line'></span>
                 <span className='mid-line'></span>
                 <span className='bottom-line'></span>
