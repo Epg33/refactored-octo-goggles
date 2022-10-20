e5b3291e61255e5cb9954e6a4e168b2ca7362266
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-10-20 13:01:23 -0500

    responsive nav, mejorar diseño

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -4,39 +4,16 @@ import {NavLink} from 'react-router-dom'
 import '../styles/nav.css'
 
 function Nav() {
-  const boton = useRef();
-  const theClass = () => {
-    boton.current.classList.toggle('open')
-  } 
-  if(window.innerWidth<550){
-    return(
-      <>
-        <nav className='nav'>
-          <div className='burger-container'>
-            <div className='burguer'>
-              <button onClick={theClass} ref={boton}>
-                <span className='top-line'></span>
-                <span className='mid-line'></span>
-                <span className='bottom-line'></span>
-              </button>
-            </div>
-          </div>
-        </nav>
-      </>
-    )
-  }
-  else{
   return (
     <nav className='nav'>
       <div className='links-container'>
         <NavLink className='nav-link' to='/'>Home</NavLink>
         <NavLink className='nav-link' to='/register'>Registro</NavLink>
         <NavLink className='nav-link' to='/login'>Log in</NavLink>
-        
       </div>
     </nav>
   )
-  }
+  
 }
 
 export default Nav
