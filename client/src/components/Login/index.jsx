f542afbbfd9f8acfd484f9574883c78a9d9f2496
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-10-20 12:43:25 -0500

    reutilizacion de estilos

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -1,13 +1,13 @@
-import React, {useRef, useContext} from "react";
-import {MdEmail, MdLock} from 'react-icons/md'
+import React, { useRef /*useContext*/ } from "react";
+import { MdEmail, MdLock } from "react-icons/md";
 import Nav from "../Nav";
 // import {AuthContext,AuthContextProvider} from '../context/AuthContext'
-import axios from 'axios'
-import {useNavigate} from 'react-router-dom'
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import "../../styles/login/login.css";
 
 function Index() {
-  const regEx = new RegExp("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$")
+  const regEx = new RegExp("[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$");
   const email = useRef();
   const password = useRef();
   const navigate = useNavigate();
@@ -15,59 +15,66 @@ function Index() {
 
   const validateLogin = (e) => {
     e.preventDefault();
-    if(!regEx.test(email.current.value)){
-      alert("ingrese un correo valido")
+    if (!regEx.test(email.current.value)) {
+      alert("ingrese un correo valido");
+    } else {
+      handleRequestOfLogin();
     }
-    else {
-      handleRequestOfLogin()
-    }
-  }
+  };
 
-  const handleLoginResposeErr = (res) =>{
-    if(res.response.status===409){
-        alert('contraseña insorrecta')
-    } else if(res.response.status===410){
-        alert('ingrese un usuario valido')
+  const handleLoginResposeErr = (res) => {
+    if (res.response.status === 409) {
+      alert("contraseña insorrecta");
+    } else if (res.response.status === 410) {
+      alert("ingrese un usuario valido");
     }
-  }
+  };
 
-  const handleLoginRespose = () =>{
-    alert('usuario logueado exitosamente');
+  const handleLoginRespose = () => {
+    alert("usuario logueado exitosamente");
     window.setTimeout(() => {
       navigate("/account/tasks", { replace: true });
       window.clearTimeout();
-    }, 2000)
-  }
+    }, 2000);
+  };
 
   const handleRequestOfLogin = () => {
-    axios.post('http://localhost:4000/login', {
-      email: email.current.value,
-      password: password.current.value
-    })
-    .then(res =>{
-      console.log(res)
-      window.localStorage.setItem('AuthTokenForLoginInThisSpecificApp', res.data.token)
-      console.log(window.localStorage.getItem('AuthTokenForLoginInThisSpecificApp'))
-      handleLoginRespose()
-    })
-    .catch(err=>handleLoginResposeErr(err))
-  }
+    axios
+      .post("http://localhost:4000/login", {
+        email: email.current.value,
+        password: password.current.value,
+      })
+      .then((res) => {
+        console.log(res);
+        window.localStorage.setItem(
+          "AuthTokenForLoginInThisSpecificApp",
+          res.data.token
+        );
+        console.log(
+          window.localStorage.getItem("AuthTokenForLoginInThisSpecificApp")
+        );
+        handleLoginRespose();
+      })
+      .catch((err) => handleLoginResposeErr(err));
+  };
 
   return (
     <>
       <Nav />
-      <div className='login-body'>
-        <form className='login-form'>
-          <h2 className='login-title'>Log in</h2>
-          <label>
-            <MdEmail className="login-icon" />
-            <input type="email" className='login-input' placeholder="Correo: " required ref={email} />
-          </label>
-          <label>
-            <MdLock className="login-icon" />
-            <input type="password" className='login-input' placeholder="Contraseña: " required ref={password}/>
-          </label>
-          <input type="submit" className='login-submit' onClick={validateLogin}/>
+      <div className="login-body">
+        <form className="login-form">
+          <h2 className="login-title">Log in</h2>
+          <div className="login-inputs-container">
+            <label className="login-label">
+              <MdEmail className="login-icon" />
+              <input type="email" className="login-input" placeholder="Correo: " required ref={email} />
+            </label>
+            <label className="login-label">
+              <MdLock className="login-icon" />
+              <input type="password" className="login-input" placeholder="Contraseña: " required ref={password} />
+            </label>
+            <input type="submit" className="login-submit" onClick={validateLogin} />
+          </div>
         </form>
       </div>
     </>
