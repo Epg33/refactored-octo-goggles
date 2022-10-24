d3bd1d2b534d5861f1c702d39e11b30571b8599b
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-10-24 14:41:38 -0500

    corregir el eliminar tareas

--- client/src/components/Tasks/ViewTask.jsx
+++ client/src/components/Tasks/ViewTask.jsx
@@ -2,18 +2,17 @@ import React from "react";
 import "../../styles/tasks/viewTask.css";
 import { deleteTask } from "./deleteTask";
 
-function ViewTask({userid,obj, taskDependency, setTaskDependency}) {
+function ViewTask({ userid, obj, taskDependency, setTaskDependency }) {
   const dell = () => {
-    
-      deleteTask(userid, obj.id, taskDependency, setTaskDependency)
-      
-  }
+    deleteTask(userid, obj.id, taskDependency, setTaskDependency);
+  };
   return (
     <div key={obj._id}>
       <h3>{obj.title}</h3>
       <p>{obj.description}</p>
       <p>{obj.done ? "hecha" : "pendiente"}</p>
-      <button onClick={dell/*deleteTask(userid, obj.id, taskDependency, setTaskDependency)*/}>Borrar</button>
+      <button>Actualizar</button>
+      <button onClick={dell}>Borrar</button>
     </div>
   );
 }
